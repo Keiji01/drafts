aa55c00bd8923eac27520886c78080eb25c07ed7
Lucas Keiji <you@example.com>
2022-06-09 10:13:59 -0300

    adicao do flask server

---
+++ run_flask_server/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/sampleurl', methods = ['GET'])
+def samplefunction():
+    #access your DB get your results here
+    data = {"data":"Processed Data"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    port = 8000 #the custom port you want
+    app.run(host='0.0.0.0', port=port)
